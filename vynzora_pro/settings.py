3132ffa770dfcb413664e1fc6bafba97bf666150
asif <muhammedasifs321@gmail.com>
2025-03-26 21:05:58 +0530

    sitemap.xml

--- vynzora_pro/settings.py
+++ vynzora_pro/settings.py
@@ -38,8 +38,27 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'vynzora_app',
 ]
+
+SITE_ID = 1
+SITE_DOMAIN = "https://vynzora.com"
+
+
+SECURE_SSL_REDIRECT = True  # Redirect all HTTP requests to HTTPS
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# Ensure secure cookies
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# HSTS (HTTP Strict Transport Security)
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- vynzora_pro/urls.py
+++ vynzora_pro/urls.py
@@ -14,16 +14,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from vynzora_app.sitemap import StaticViewSitemap, BlogSitemap, CategorySitemap, WebsiteSitemap
 
+sitemaps = {
+    'static': StaticViewSitemap,
+    'blogs': BlogSitemap,
+    'categories': CategorySitemap,
+    'websites': WebsiteSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vynzora_app.urls')),
+     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = 'vynzora_app.views.page_404'
