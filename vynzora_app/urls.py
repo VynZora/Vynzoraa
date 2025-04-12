575c1c0661d3731e805cb292ba68481a18f9c5df
asif <muhammedasifs321@gmail.com>
2025-04-12 14:20:47 +0530

    index.html

--- vynzora_app/urls.py
+++ vynzora_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 
     # User Side
     path('', views.index, name='index'),
+    path('index.html/', views.index, name='index_html'), 
     path('about/', views.about, name='about'),
     path('portfolio/', views.portfolio, name='portfolio'),
     path('contact/', views.contact, name='contact'),
